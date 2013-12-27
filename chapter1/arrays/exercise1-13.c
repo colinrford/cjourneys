7eeb7e4c0106c3248ebd554865e070b1e8019c03
Colin Ford <ford31@purdue.edu>
2013-12-26 23:18:34 -0500

    Made more progress I think? maybe? who knows. I am no good at this stuff.

--- chapter1/arrays/exercise1-13.c
+++ chapter1/arrays/exercise1-13.c
@@ -8,51 +8,51 @@
 
 main()
 {
-  int c, i, j, nw, lc, state;
-  int wordlength[WORDLEN + 1];
+    int c, i, j, k, nw, lc, state;
+    int wordlength[WORDLEN + 1];
 
-  state = OUT;
-  nw = lc = 0;
-  for (i = 0; i < WORDLEN; ++i)
-    wordlength[i] = 0;
-  while ((c = getchar()) != EOF)
-  {
-    if (c == ' ' || c == '\n' || c == '\t')
+    state = OUT;
+    nw = lc = 0;
+    for (i = 0; i < WORDLEN; ++i)
+        wordlength[i] = 0;
+    while ((c = getchar()) != EOF)
     {
-      state = OUT;
-      lc = 0;
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            state = OUT;
+            if (lc == 0)
+                break;
+            else if (lc < WORDLEN)
+                ++wordlength[lc];
+            else if (lc >= WORDLEN)
+                ++wordlength[10];
+            lc = 0;
+        }
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+            lc = 0;
+        }
+        if (state == IN)
+        {
+            ++lc;
+        }
     }
-    else if (state == OUT)
+    for (j = 0; j < WORDLEN; ++j)
     {
-      state = IN;
-      ++nw;
-      lc = 0;
+        k = 0;
+        printf("%d:", (j + 1));
+        while (k <= wordlength[j])
+        {
+            if (wordlength[j] == 0)
+                break;
+            else 
+            {
+                printf("*");
+                ++k;
+            }
+        }
+        printf("\n");
     }
-    if (state == IN)
-    {
-      ++lc;
-      if (lc == 0)
-        ;
-      else if (lc <= WORDLEN)
-        ++wordlength[lc];
-      else if (lc > WORDLEN)
-        ++wordlength[10];
-    }
-  }
-  for (j = 0; j < WORDLEN; ++j)
-  {
-    int k = 0;
-    printf("%d:", (j + 1));
-    while (k <= wordlength[j])
-    {
-      if (wordlength[j] == 0)
-        break;
-      else 
-      {
-        printf("*");
-        ++k;
-      }
-    }
-    printf("\n");
-  }
 }
