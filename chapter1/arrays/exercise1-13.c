feced6d2d4a4739b82c853cae488d494129b97e0
Colin Ford <ford31@purdue.edu>
2014-01-04 15:29:49 -0500

    Not sure why there is an infinite loop. Very frustrating.

--- chapter1/arrays/exercise1-13.c
+++ chapter1/arrays/exercise1-13.c
@@ -2,34 +2,48 @@
 
 #define IN 1 // inside a word
 #define OUT 0 // outside a word
+#define WORDLEN 10 // maximum word length
 
 /* write a program to print a histogram of the lengths of words in its input. It is easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging. */
 
 main()
 {
-  int c, i, nw, lc, state;
+  int c, i, j, nw, lc, state;
+  int wordlength[WORDLEN + 1];
 
   state = OUT;
   nw = lc = 0;
+  for (i = 0; i < WORDLEN; ++i)
+    wordlength[i] = 0;
   while ((c = getchar()) != EOF)
   {
-    if (state == IN)
-    {
-      printf("*");
-    }
-      if (c == ' ' || c == '\n' || c == '\t')
+    if (c == ' ' || c == '\n' || c == '\t')
     {
       state = OUT;
-      printf("\n");
     }
     else if (state == OUT)
     {
       state = IN;
       ++nw;
-      printf("%d: ", nw);
+    }
+    if (state == IN)
+    {
+      ++lc;
+      if (lc <= WORDLEN)
+        ++wordlength[lc];
+      else if (lc > WORDLEN)
+        ++wordlength[10];
     }
   }
-  int wordlength[nw];
-  for (i = 0; i < nw; ++i)
-    wordlength[i] = 0;
+  for (j = 0; j <= WORDLEN; ++j)
+  {
+    int k = 0;
+    printf("%d:", (j + 1));
+    while (k <= wordlength[j])
+    {
+      printf("*");
+      ++k;
+    }
+    printf("\n");
+  }
 }
