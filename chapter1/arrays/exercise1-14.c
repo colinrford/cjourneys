4a51e043e1c6287b2a29faa4e71cd60007e3bb43
Colin Ford <ford31@purdue.edu>
2014-01-11 21:04:15 -0500

    pretty much finished exercise 1-14. Haven't been able to put much time into this for the past week. Will begin working more once the semester starts.

--- chapter1/arrays/exercise1-14.c
+++ chapter1/arrays/exercise1-14.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
+#define MAXCHARNUM 256 // max number of characters in ASCII
+
 /* Write a program to print a histogram of the frequencies of different characters in its input */
 
 main()
 {
+  int c, i, j;
+  int characters[MAXCHARNUM + 1]; // if in other character sets
 
+  while ((c = getchar()) != EOF)
+  {
+    characters[c]++;
+  }
+
+  for (i = 0; i < MAXCHARNUM; i++)
+  {
+    printf("%d: ", i);
+    if (characters[i] != 0)
+    {
+      for (j = 0; j < characters[i]; j++)
+      {
+        printf("*");
+      }
+    }
+    printf("\n");
+  }
 }
